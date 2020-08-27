fc5a57f843f3912df0c3ea7d96b4281758316c81
pratikdaigavane <daigavanep@gmail.com>
2020-08-27 09:06:42 +0530

    Logger added

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
-const express = require('express')
 
-const app = express()
+const express = require('express')
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const logger = pino({
+    level: process.env.LOG_LEVEL || 'info',
+    prettifier: require('pino-colada')
+});
+
+const expressLogger = expressPino({logger});
 const port = parseInt(process.env.port)
+const app = express()
+
+app.use(expressLogger);
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
+    logger.info(`Listening at http://localhost:${port}`)
 })
