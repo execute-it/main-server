4a4324dae1eef3fb2f81daa424f6e4ba929d1b80
pratikdaigavane <daigavanep@gmail.com>
2020-08-29 11:35:14 +0530

    mongodb integrated

--- app.js
+++ app.js
@@ -30,16 +30,16 @@ app.get('/', (req, res) => {
         })
 
 })
-//
-// app.post('/rooms', async (req, res) => {
-//     const room = await docker.createRoom(
-//         process.env.USER_SERVER_IMAGE,
-//         uuidv4(),
-//         process.env.USER_SERVER_MEM_LIMIT,
-//         process.env.USER_SERVER_CPU_LIMIT,
-//         process.env.TRAEFIK_HOST, process.env.USER_SERVER_NETWORK)
-//     res.json(room)
-// })
+
+app.post('/rooms', async (req, res) => {
+    const room = await docker.createRoom(
+        process.env.USER_SERVER_IMAGE,
+        uuidv4(),
+        process.env.USER_SERVER_MEM_LIMIT,
+        process.env.USER_SERVER_CPU_LIMIT,
+        process.env.TRAEFIK_HOST, process.env.USER_SERVER_NETWORK)
+    res.json(room)
+})
 
 app.listen(port, () => {
     logger.info(`Listening at http://localhost:${port}`)
