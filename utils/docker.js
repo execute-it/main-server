54a3fa3fadf52fbc5e21613554b201b37102e46b
pratikdaigavane <daigavanep@gmail.com>
2020-09-06 00:00:52 +0530

    bug fixes

--- utils/docker.js
+++ utils/docker.js
@@ -1,7 +1,7 @@
 const Docker = require('dockerode');
 const logger = require('./logger');
 
-createRoom = async (image, roomId, memLimit, cpuLimit, host, network) => {
+createRoom = async(image, roomId, memLimit, cpuLimit, host, network) => {
     const docker = new Docker({ socketPath: process.env.DOCKER_SOCKET });
     let auxContainer;
     let res = {};
@@ -16,30 +16,36 @@ createRoom = async (image, roomId, memLimit, cpuLimit, host, network) => {
             [`traefik.http.routers.${roomId}.rule`]: "Host(`" + host + "`) && PathPrefix(`/" + roomId + "`)",
             [`traefik.http.middlewares.${roomId}-stripprefix.stripprefix.prefixes`]: "/" + roomId,
             [`traefik.http.middlewares.${roomId}-auth.forwardauth.address`]: `http://localhost/auth`,
-            [`traefik.http.middlewares.${roomId}-ratelimit.ratelimit.average`]:"100",
-            [`traefik.http.middlewares.${roomId}-ratelimit.ratelimit.burst`]:"50",
+            [`traefik.http.middlewares.${roomId}-ratelimit.ratelimit.average`]: "100",
+            [`traefik.http.middlewares.${roomId}-ratelimit.ratelimit.burst`]: "50",
             [`traefik.http.routers.${roomId}.middlewares`]: `${roomId}-stripprefix, ${roomId}-auth, ${roomId}-ratelimit`,
-            [`traefik.http.middlewares.${roomId}-auth.forwardauth.trustForwardHeader`]:"true"      
+            [`traefik.http.middlewares.${roomId}-auth.forwardauth.trustForwardHeader`]: "true"
         },
         Tty: true,
+        Env: [`PROJECT_ID=2233`, `OT_SERVER_URL=${process.env.OT_SERVER_URL}`, `OT_API_KEY=${process.env.OT_API_KEY}`],
         OpenStdin: false,
         StdinOnce: false,
+        // HostConfig: {
+        //     "Memory": 26435456,
+        //     "CpuPeriod": 100000,
+        //     "CpuQuota": 25000
+        // },
         NetworkingConfig: {
             EndpointsConfig: {
                 [network]: {}
             }
         }
-    }).then(function (container) {
+    }).then(function(container) {
         logger.info(`starting docker container for room ${roomId} from image ${image}`)
         auxContainer = container;
         return auxContainer.start();
-    }).then(function () {
+    }).then(function() {
         logger.info(`started docker container for room ${roomId} from image ${image}`)
         res = {
             "status": "created",
-            "roomURL": `http://${host}/${roomId}`
+            "roomURL": `${host}/${roomId}`
         }
-    }).catch(function (err) {
+    }).catch(function(err) {
         logger.error(`error occurred while creating for room ${roomId} from image ${image} ${err}`)
         res = {
             "status": "error"
@@ -52,4 +58,3 @@ createRoom = async (image, roomId, memLimit, cpuLimit, host, network) => {
 // createRoom('pratik', 'cvbn', '', '', 'localhost', 'executeit')
 
 exports.createRoom = createRoom
-
