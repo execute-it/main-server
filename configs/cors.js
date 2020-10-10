4fa02d94473167acc6f5fcfb103d8d60a04448d1
pnshiralkar <pnshiralkar@gmail.com>
2020-10-10 23:16:22 +0530

    Restricted CORS for executeit.ml, no restriction for dev environment

---
+++ configs/cors.js
@@ -0,0 +1,13 @@
+const { model } = require("../models/Room")
+
+// Configure CORS whitelisted domains here
+let corsWhitelist = ['https://executeit.ml']
+
+// All domains (*) allowed if NODE_ENV is dev
+corsWhitelist = process.env.NODE_ENV === 'dev' ? '*' : corsWhitelist
+
+let corsOptions = {
+  origin: corsWhitelist
+}
+
+module.exports = corsOptions;
