8e1e32678c802cf5f42ecaaa614989339926f96f
pratikdaigavane <daigavanep@gmail.com>
2020-10-23 09:34:52 +0530

    cors added for localhost

--- configs/cors.js
+++ configs/cors.js
@@ -1,13 +1,13 @@
 const { model } = require("../models/Room")
 
 // Configure CORS whitelisted domains here
-let corsWhitelist = ['https://executeit.ml', 'https://www.executeit.ml']
+let corsWhitelist = ['https://executeit.ml', 'https://www.executeit.ml', 'http://localhost:3000']
 
 // All domains (*) allowed if NODE_ENV is dev
 corsWhitelist = process.env.NODE_ENV === 'dev' ? '*' : corsWhitelist
 
 let corsOptions = {
-  origin: corsWhitelist
+    origin: corsWhitelist
 }
 
 module.exports = corsOptions;
