7b299825bd5816de2c8c2cd6d19b1dcfd9561051
Prathamesh Shiralkar <47828215+pnshiralkar@users.noreply.github.com>
2023-08-07 16:17:03 +0530

    Update cors.js executeit.app

--- configs/cors.js
+++ configs/cors.js
@@ -1,7 +1,7 @@
 const { model } = require("../models/Room")
 
 // Configure CORS whitelisted domains here
-let corsWhitelist = ['https://executeit.ml', 'https://www.executeit.ml', 'http://localhost:3000', 'https://pict.sudolms.in']
+let corsWhitelist = ['https://executeit.app', 'https://www.executeit.app', 'http://localhost:3000', 'https://pict.sudolms.in']
 
 // All domains (*) allowed if NODE_ENV is dev
 corsWhitelist = process.env.NODE_ENV === 'dev' ? '*' : corsWhitelist
